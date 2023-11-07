34b5ee53863f1a99799dc38cf80c29d736d3a82a
Christina Larsen <clarsen1782@outlook.com>
2023-11-06 18:56:48 -0800

    More Setup and Connection

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.get('/', (req, res) => {
+    if (req.session.views) {
+        req.session.views++;
+    } else {
+        req.session.views = 1;
+    }
+    res.send(`You've visited this page ${req.session.views} times.`);
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
