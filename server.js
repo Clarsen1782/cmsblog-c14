c2498a89460a190abeccbcafd18a59517797ec47
Christina Larsen <clarsen1782@outlook.com>
2023-11-07 21:08:49 -0800

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require('./config/connection')
+const sequelize = require('./config/connection');
 
 const hbs = exphbs.create();
 const PORT = process.env.PORT || 3001;
