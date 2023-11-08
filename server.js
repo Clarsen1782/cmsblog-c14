2393206597dbbe08a394169ec60a8a2f95aa524b
Christina Larsen <clarsen1782@outlook.com>
2023-11-07 18:37:33 -0800

    Rework Connection

--- server.js
+++ server.js
@@ -1,44 +1,18 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const routes = require('./controllers/api');
+const routes = require('./controllers');
+const sequelize = require('./config/connection')
 
-const app = express();
+const hbs = exphbs.create();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-const hbs = exphbs.create({ helpers });
-
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
-app.use(session(sess));
-app.use('/', routes);
-
-app.get('/', (req, res) => {
-    if (req.session.views) {
-        req.session.views++;
-    } else {
-        req.session.views = 1;
-    }
-    res.send(`You've visited this page ${req.session.views} times.`);
-});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.unlencoded({ extend: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
