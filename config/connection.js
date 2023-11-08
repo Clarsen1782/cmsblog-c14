fb4adfa63a3b7ba4717f1b61050fecfca2a9bc9a
Christina Larsen <clarsen1782@outlook.com>
2023-11-07 22:49:28 -0800

    Broken WTF

--- config/connection.js
+++ config/connection.js
@@ -8,14 +8,13 @@ if (process.env.JAWSDB_URL) {
 } else {
   console.log("using local db");
   sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-          host: 'localhost',
-          dialect: 'mysql',
-          port: 3306
-      }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+    }
   )
 }
 
